d0b7a544bc2f814cecc6cd2b6bcf432260e1a96b
Michael Matuzak <mmatuzak@gmail.com>
2019-12-30 21:12:33 +0000

    formatting

--- c-playground/hello-world/hello-world.c
+++ c-playground/hello-world/hello-world.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-	printf("hello, world\n");
+  printf("hello, world\n");
   debug("hello, world\n");
   log_info("what the info!\n");
   log_warn("what the warn!\n");
   log_err("uh oh!\n");
-	return 0;
+  return 0;
 }
