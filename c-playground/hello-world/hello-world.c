a8d56739bc561be68cb07f5dfeab2154fc7da283
Michael Matuzak <mmatuzak@gmail.com>
2019-12-30 21:11:14 +0000

    add c example

---
+++ c-playground/hello-world/hello-world.c
@@ -0,0 +1,11 @@
+#include "../dbg.h"
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	printf("hello, world\n");
+  debug("hello, world\n");
+  log_info("what the info!\n");
+  log_warn("what the warn!\n");
+  log_err("uh oh!\n");
+	return 0;
+}
